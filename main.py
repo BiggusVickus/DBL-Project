bb1b02ca02ecda83f0cd4af4ad96e656365b06c9
Victor Piaskowski <vicpias@yahoo.com>
2020-05-06 20:20:05 +0200

    added visualization explanation

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def index_home():
 		# the redirect can be to the same route or somewhere else
 		return redirect(url_for('index'))
 	# show the form, it wasn't submitted
-	return render_template('index.html', )
+	return render_template('index.html')
 
 @app.route('/vis1', methods=['GET', 'POST'])
 def index_vis1():
@@ -60,8 +60,9 @@ def index_vis1():
 		# the redirect can be to the same route or somewhere else
 		return redirect(url_for('vispage1'))
 	# show the form, it wasn't submitted
-	visualization = 1
-	return render_template('vispage.html', vis_page = visualization)
+	vis_page = 1
+	vis_text = 'The concept of this graph is to show the user the difference between the scanpath of a colored graph and a noncolored graph. By default the colored image shows on screen and there are checkmarks that allow the user to select if they want to see just the colored scanpath, the black and white, both, or none, with a legend of course. The user can then change the maps as they please.'
+	return render_template('vispage.html', vis_page = vis_page, vis_text=vis_text)
 
 @app.route('/vis2', methods=['GET', 'POST'])
 def index_vis2():
@@ -71,8 +72,9 @@ def index_vis2():
 		# the redirect can be to the same route or somewhere else
 		return redirect(url_for('vispage'))
 	# show the form, it wasn't submitted
-	visualization = 2
-	return render_template('vispage.html', vis_page = visualization)
+	vis_page = 2
+	vis_text = 'The goal of this visualization is to show the user how the x and y poition changes with respect to time. The user can see 3 individual graphs. The first graph shows the (x, y) position as the user scans the image. The second graph that the user can see is an (x, time) graph, where as time goes on, the x axis reflects the change in x position of the eyes, thile the y axis reflects the time spent. The thid graph is the opposite, ie the user sees the (time, y) graph. As time moves on the x-axis, the user sees how the y poition of the gazepath changes.'
+	return render_template('vispage.html', vis_page = vis_page, vis_text = vis_text)
 
 @app.route('/vis3', methods=['GET', 'POST'])
 def index_vis3():
@@ -82,8 +84,9 @@ def index_vis3():
 		# the redirect can be to the same route or somewhere else
 		return redirect(url_for('vispage'))
 	# show the form, it wasn't submitted
-	visualization = 3
-	return render_template('vispage.html', vis_page = visualization)
+	vis_page = 3
+	vis_text = 'The idea behind this visualization is that the user can see a heatmap of a specific user, map, and map color. It helps  the user understand the density of where the majority of the data is using a fun interactive colorcoding.'
+	return render_template('vispage.html', vis_page = vis_page, vis_text=vis_text)
 
 @app.route('/vis4', methods=['GET', 'POST'])
 def index_vis4():
@@ -92,10 +95,11 @@ def index_vis4():
 		# redirect to end the POST handling
 		# the redirect can be to the same route or somewhere else
 		return redirect(url_for('vispage'))
-	visualization = 4
+	vis_page = 4
+	vis_text = 'The idea behind this visualization is that you can see....'
 	# show the form, it wasn't submitted
 
-	return render_template('vispage.html', vis_page = visualization)
+	return render_template('vispage.html', vis_page = vis_page, vis_text=vis_text)
 
 
 
