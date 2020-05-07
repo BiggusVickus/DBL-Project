a03ef75a75a7239bfe2fceeec6d3a58f56b20731
Victor Piaskowski <vicpias@yahoo.com>
2020-05-08 01:59:43 +0200

    Added randomly selected color images to homepage
    Front page now randomly selects a colored image to display on the front page. Did some code changes, and layout stuff.

--- README.md
+++ README.md
@@ -3,5 +3,16 @@
 To get the local server to work, you need to go to the terminal window. 
 First activate the virtual environment by typing ```. venv/bin/activate```. 
 Then you have to do ```pip install flask```
-Then to run the server, type ```python main.py```. Some stuff appears in the terminal window. click on ```http://127.0.0.1:5000/``` (or just copy paste this) and it will open up a new window on chrome or whatever. You can upload a jpeg, pdf, csv, png file. The file will then be pushed to the "Uploads" folder. I also added a functionality that we cn go to different pages for different visualizations. 
+Then to run the server, type ```python main.py```.  
+Some stuff appears in the terminal window. Click on ```http://127.0.0.1:5000/``` (or just copy paste this) and it will open up a new window on chrome or whatever. You can upload a jpeg, pdf, csv, png file. The file will then be uploaded to ```/static/Uploads```. On the side are links to different visualizations (no graphs yet :(.. ) Also whenever you visit the home page, a colored image is randomly selected and put on the background (still working on the background part).  
+
 Please create a new branch from main so that any changes made are not reflected upon the whole branch. Then work in that branch, and when you are ready to push a stable change, like adding a functionality that works, then you may push to master. I (Victor) will review it and accept it. 
+
+Structure of the project:  
+- ```pycache``` has nothing interesting.  Neither really ```.vscode``` or ```venv```.  
+You don't have to do anything in app.py.  
+
+- ```Static``` holds all files that flask will have to access. Inside is the ```CSS``` folder for all CSS files,  ```Stimuli``` folder for all images given to us, plus everytime a user visits the homepage a new color image is randomly selected, ```Uploads``` folder is for user uploads, and ```Visualizations``` is for the visualization files.  
+- ```templates``` holds all of the HTML files.  
+- ```Main.py``` is where the magic happens. This handles all of the python code.  
+- ```test.py``` is where I dump all of my changes, tinker with shit, or if I need to chnage something that I fucked up a long time ago, thats where I copy-paste what I have at the time of noticing the fuck up.   
--- main.py
+++ main.py
@@ -1,26 +1,34 @@
+#imports
 import os
-#import magic
+import glob
+import random
 import urllib.request
 from app import app
 from flask import Flask, flash, request, redirect, render_template
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = './Uploads'
+#DON"T TOUCH
+UPLOAD_FOLDER = 'static/Uploads'
 app = Flask(__name__)
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
-
+#Remove pdf later
 ALLOWED_EXTENSIONS = set(['csv', 'pdf', 'png', 'jpg', 'jpeg'])
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 	
+#randomly selects a colored image from /static/Stimuli then passes it onto the html page
 @app.route('/')
 def upload_form():
-	return render_template('index.html')
+	image_list = [name for name in glob.glob('static/Stimuli/*[0-9]_*')]
+	random_number = random.randint(0, len(image_list)-1)
+	image_for_background = str(image_list[random_number])
+	return render_template('index.html', image_for_background = image_for_background)
 
+	#does the upload function, absolutely dont touch or else it breaks
 @app.route('/', methods=['POST'])
 def upload_file():
 	if request.method == 'POST':
@@ -41,7 +49,8 @@ def upload_file():
 			flash('Allowed file types are csv, png, jpg, and jpeg')
 			return redirect(request.url)
 
-@app.route('/home', methods=['GET', 'POST'])
+#part of the homepage
+@app.route('/', methods=['GET', 'POST'])
 def index_home():
 	if request.method == 'POST':
 		# do stuff when the form is submitted
@@ -51,14 +60,11 @@ def index_home():
 	# show the form, it wasn't submitted
 	return render_template('index.html')
 
+#first visualization, returns the visualization number and a summary. Repeat for below. 
 @app.route('/vis1', methods=['GET', 'POST'])
 def index_vis1():
 	if request.method == 'POST':
-		# do stuff when the form is submitted
-		# redirect to end the POST handling
-		# the redirect can be to the same route or somewhere else
 		return redirect(url_for('vispage1'))
-	# show the form, it wasn't submitted
 	vis_page = 1
 	vis_text = 'The concept of this graph is to show the user the difference between the scanpath of a colored graph and a noncolored graph. By default the colored image shows on screen and there are checkmarks that allow the user to select if they want to see just the colored scanpath, the black and white, both, or none, with a legend of course. The user can then change the maps as they please.'
 	return render_template('vispage.html', vis_page = vis_page, vis_text=vis_text)
@@ -66,11 +72,7 @@ def index_vis1():
 @app.route('/vis2', methods=['GET', 'POST'])
 def index_vis2():
 	if request.method == 'POST':
-		# do stuff when the form is submitted
-		# redirect to end the POST handling
-		# the redirect can be to the same route or somewhere else
 		return redirect(url_for('vispage'))
-	# show the form, it wasn't submitted
 	vis_page = 2
 	vis_text = 'The goal of this visualization is to show the user how the x and y poition changes with respect to time. The user can see 3 individual graphs. The first graph shows the (x, y) position as the user scans the image. The second graph that the user can see is an (x, time) graph, where as time goes on, the x axis reflects the change in x position of the eyes, thile the y axis reflects the time spent. The thid graph is the opposite, ie the user sees the (time, y) graph. As time moves on the x-axis, the user sees how the y poition of the gazepath changes.'
 	return render_template('vispage.html', vis_page = vis_page, vis_text = vis_text)
@@ -78,11 +80,7 @@ def index_vis2():
 @app.route('/vis3', methods=['GET', 'POST'])
 def index_vis3():
 	if request.method == 'POST':
-		# do stuff when the form is submitted
-		# redirect to end the POST handling
-		# the redirect can be to the same route or somewhere else
 		return redirect(url_for('vispage'))
-	# show the form, it wasn't submitted
 	vis_page = 3
 	vis_text = 'The idea behind this visualization is that the user can see a heatmap of a specific user, map, and map color. It helps  the user understand the density of where the majority of the data is using a fun interactive colorcoding.'
 	return render_template('vispage.html', vis_page = vis_page, vis_text=vis_text)
@@ -90,17 +88,12 @@ def index_vis3():
 @app.route('/vis4', methods=['GET', 'POST'])
 def index_vis4():
 	if request.method == 'POST':
-		# do stuff when the form is submitted
-		# redirect to end the POST handling
-		# the redirect can be to the same route or somewhere else
 		return redirect(url_for('vispage'))
 	vis_page = 4
 	vis_text = 'The idea behind this visualization is that you can see....'
-	# show the form, it wasn't submitted
-
 	return render_template('vispage.html', vis_page = vis_page, vis_text=vis_text)
 
 
-
+#starts the app in debug mode, hit refresh to see an updated page, all without restarting the server. 
 if __name__ == "__main__":
 	app.run(debug=True)
