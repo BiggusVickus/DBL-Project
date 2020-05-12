6acb60d22b9898f021d92487ebcf182d6a19753b
MarkvanderMeer <64903512+MarkvanderMeer@users.noreply.github.com>
2020-05-12 15:04:50 +0200

    Create Vis4.py

---
+++ static/Visualizations/Vis4.py
@@ -0,0 +1,12 @@
+import numpy as np  # import auxiliary library, typical idiom
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from bokeh.layouts import gridplot
+from bokeh.plotting import figure, output_file, show, curdoc
+from bokeh.io import output_file, show
+from bokeh.models import ColumnDataSource, Select, Tabs, Panel, Button, ImageURL
+from bokeh.layouts import column, row, WidgetBox, layout
+from bokeh.models.callbacks import CustomJS
+
+df_map = pd.read_csv('static/Uploads/fixation_data.csv', parse_dates=[0])
