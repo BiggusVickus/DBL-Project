cf62e56e893b76745de3126b02f273e41657a4be
Victor Piaskowski <vicpias@yahoo.com>
2020-05-20 20:44:27 +0200

    Multiple file support and added favicon

--- main.py
+++ main.py
@@ -29,25 +29,24 @@ def upload_form():
 	return render_template('index.html', image_for_background = image_for_background)
 
 	#does the upload function, absolutely dont touch or else it breaks
+
 @app.route('/', methods=['POST'])
 def upload_file():
 	if request.method == 'POST':
-		# check if the post request has the file part
-		if 'file' not in request.files:
+        # check if the post request has the files part
+		if 'files[]' not in request.files:
 			flash('No file part')
 			return redirect(request.url)
-		file = request.files['file']
-		if file.filename == '':
-			flash('No file selected for uploading')
-			return redirect(request.url)
-		if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			flash('File successfully uploaded')
-			return redirect('/')
-		else:
-			flash('Allowed file types are csv, png, jpg, and jpeg')
-			return redirect(request.url)
+		files = request.files.getlist('files[]')
+		for file in files:
+			if file and allowed_file(file.filename):
+				filename = secure_filename(file.filename)
+				file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			else:
+				flash('Allowed file types are csv, png, jpg, and jpeg')
+		flash('File(s) successfully uploaded')
+		return redirect(request.url)
+		
 
 #part of the homepage
 @app.route('/', methods=['GET', 'POST'])
@@ -65,7 +64,7 @@ def index_home():
 def index_vis1():
 	if request.method == 'POST':
 		return redirect(url_for('vispage1'))
-	vis_page = 1
+	vis_page = 'Visualization 1'
 	vis_text = 'The concept of this graph is to show the user the difference between the scanpath of a colored graph and a noncolored graph. By default the colored image shows on screen and there are checkmarks that allow the user to select if they want to see just the colored scanpath, the black and white, both, or none, with a legend of course. The user can then change the maps as they please.'
 	return render_template('vispage.html', vis_page = vis_page, vis_text=vis_text)
 
@@ -73,7 +72,7 @@ def index_vis1():
 def index_vis2():
 	if request.method == 'POST':
 		return redirect(url_for('vispage'))
-	vis_page = 2
+	vis_page = 'Visualization 2'
 	vis_text = 'The goal of this visualization is to show the user how the x and y poition changes with respect to time. The user can see 3 individual graphs. The first graph shows the (x, y) position as the user scans the image. The second graph that the user can see is an (x, time) graph, where as time goes on, the x axis reflects the change in x position of the eyes, thile the y axis reflects the time spent. The thid graph is the opposite, ie the user sees the (time, y) graph. As time moves on the x-axis, the user sees how the y poition of the gazepath changes.'
 	return render_template('vispage.html', vis_page = vis_page, vis_text = vis_text)
 
@@ -81,15 +80,15 @@ def index_vis2():
 def index_vis3():
 	if request.method == 'POST':
 		return redirect(url_for('vispage'))
-	vis_page = 3
-	vis_text = 'The idea behind this visualization is that the user can see a heatmap of a specific user, map, and map color. It helps  the user understand the density of where the majority of the data is using a fun interactive colorcoding.'
+	vis_page = 'Visualization 3'
+	vis_text = 'The idea behind this visualization is that the user can see a heatmap of a specific user, map, and map color. It helps  the user understand the density of where the majority of the data is using a fun interactive colorcoding. All fixation points from each user fo a specific stimuli are added to the map. Depending on how many other dots are close to a dot, the dot\'s color changes from blue to red. The denser the dots, the redder the dots will appear.'
 	return render_template('vispage.html', vis_page = vis_page, vis_text=vis_text)
 
 @app.route('/vis4', methods=['GET', 'POST'])
 def index_vis4():
 	if request.method == 'POST':
 		return redirect(url_for('vispage'))
-	vis_page = 4
+	vis_page = 'Visualization 4'
 	vis_text = 'The idea behind this visualization is that you can see....'
 	return render_template('vispage.html', vis_page = vis_page, vis_text=vis_text)
 
