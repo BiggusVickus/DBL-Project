c4ca43a89b7dfd3450d846cbf95906d3a997a7a0
MarkvanderMeer <64903512+MarkvanderMeer@users.noreply.github.com>
2020-05-20 18:13:41 +0200

    Update Vis4.py

--- static/Visualizations/Vis4.py
+++ static/Visualizations/Vis4.py
@@ -34,14 +34,12 @@ S2_color = df_map_S2_color_grouped[['FixationDuration']].sum().reset_index()
 S2_gray = df_map_S2_gray_grouped[['FixationDuration']].sum().reset_index()
 
 list_1 = S1_color['StimuliName'].tolist()
-
-#StimuliNames_S1_color = ['01_Antwerpen_S1.jpg', '02_Berlin_S1.jpg', '03_Bordeaux_S1.jpg', '04_Köln_S1.jpg', '05_Frankfurt_S1.jpg', '06_Hamburg_S1.jpg', 
-#'07_Moskau_S1.jpg', '08_Riga_S1.jpg', '09_Tokyo_S1.jpg', '10_Barcelona_S1.jpg', '11_Bologna_S1.jpg', '12_Brussel_S1.jpg', '13_Budapest_S1.jpg',
-#'14_Düsseldorf_S1.jpg', '15_Göteborg_S1.jpg', '16_Hong_Kong_S1.jpg', '17_Krakau_S1.jpg', '18_Ljubljana_S1.jpg', '19_New_York_S1.jpg', '20_Paris_S1.jpg',
-#'21_Pisa_S1.jpg', '22_Venedig_S1.jpg', '23_Warschau_S1.jpg', '24_Zürich_S1.jpg']
+list_2 = S1_gray['StimuliName'].tolist()
+list_3 = S2_color['StimuliName'].tolist()
+list_4 = S2_gray['StimuliName'].tolist()
 
 #Make ColumnDataSource
-source_city = ColumnDataSource(data = dict(x=[], y=[], timestamp=[], station=[], fixation_duration=[]))
+source_city = ColumnDataSource(data = dict(x=[], station=[], fixation_duration=[]))
 
 #Global variables
 stations = []
@@ -53,18 +51,31 @@ for station in df_map['StimuliName']:
 #Remove duplicates
 stations = list(dict.fromkeys(stations))
 
-select_city = Select(
+select_city_1 = Select(
+    title = 'Choose city',
+    value = '01_Antwerpen_S1.jpg',
+    options = stations
+)
+
+select_city_2 = Select(
     title = 'Choose city',
     value = '01_Antwerpen_S1.jpg',
     options = stations
 )
 
 def make_dataset():
-    plot_data_S1_color = S1_color[(S1_color['StimuliName'] == select_city.value)].copy()
-    #plot_data_S1_gray = df_map_S1_gray_grouped[(df_map_S1_gray_grouped['StimuliName'] == select_city.value)].copy()
-    #plot_data_S2_color = df_map_S2_color_grouped[(df_map_S2_color_grouped['StimuliName'] == select_city.value)].copy()
-    #plot_data_S2_gray = df_map_S2_gray_grouped[(df_map_S2_gray_grouped['StimuliName'] == select_city.value)].copy()
-    return plot_data_S1_color, #plot_data_S1_gray, plot_data_S2_color, plot_data_S2_gray
+    #if(df_map[df_map['StimuliName'].str.contains("S1")]): #& df_map_S1[df_map_S1['description'] == 'color']):
+        plot_data_S1_color = S1_color[(S1_color['StimuliName'] == select_city_1.value)].copy()
+        return plot_data_S1_color
+    #elif(df_map[df_map['StimuliName'].str.contains("S2")]):# & df_map_S1[df_map_S1['description'] == 'gray']):
+    #    plot_data_S1_gray = S1_gray[(S1_gray['StimuliName'] == select_city_2.value)].copy()
+    #    return plot_data_S1_gray
+    #elif(df_map[df_map['StimuliName'].str.contains("S2")] & df_map_S1[df_map_S1['description'] == 'color']):
+    #    plot_data_S2_color = S2_color[(S2_color['StimuliName'] == select_city.value)].copy()
+    #    return plot_data_S2_color
+    #elif(df_map[df_map['StimuliName'].str.contains("S2")] & df_map_S1[df_map_S1['description'] == 'gray']):
+    #    plot_data_S2_gray = S2_gray[(S2_gray['StimuliName'] == select_city.value)].copy()
+    #    return plot_data_S2_gray
 
 def make_plot(src):
     fig1 = figure(
@@ -74,32 +85,73 @@ def make_plot(src):
         plot_width = 400,
         plot_height = 400
     )
-    fig1.vbar(x='StimuliName', top = 'FixationDuration', width = 0.45, source = source_city, color = "red", legend_label = "color")
-    #fig1.vbar(x='StimuliName', top = S1_gray, width = 0.45, source = source_city, color = "blue", legend_label = "gray")
+    fig1.vbar(x='x', top = 'fixation_duration', width = 0.45, source = source_city, color = "red", legend_label = "color")
     fig1.xaxis.axis_label = 'City'
     fig1.yaxis.axis_label = 'Time'
     fig1.xgrid.grid_line_color = None
-    return fig1
+    
+    fig2 = figure(
+        x_range = list_2,
+        y_range = (0,750000),
+        title = 'Barcharts',
+        plot_width = 400,
+        plot_height = 400
+    )
+    fig2.vbar(x='x', top = 'fixation_duration', width = 0.45, source = source_city, color = "red", legend_label = "gray")
+    fig2.xaxis.axis_label = 'City'
+    fig2.yaxis.axis_label = 'Time'
+    fig2.xgrid.grid_line_color = None
+
+    fig3 = figure(
+        x_range = list_3,
+        y_range = (0,750000),
+        title = 'Barcharts',
+        plot_width = 400,
+        plot_height = 400
+    )
+    fig3.vbar(x='x', top = 'fixation_duration', width = 0.45, source = source_city, color = "red", legend_label = "color")
+    fig3.xaxis.axis_label = 'City'
+    fig3.yaxis.axis_label = 'Time'
+    fig3.xgrid.grid_line_color = None
+
+    fig4 = figure(
+        x_range = list_4,
+        y_range = (0,750000),
+        title = 'Barcharts',
+        plot_width = 400,
+        plot_height = 400
+    )
+    fig4.vbar(x='x', top = 'fixation_duration', width = 0.45, source = source_city, color = "red", legend_label = "gray")
+    fig4.xaxis.axis_label = 'City'
+    fig4.yaxis.axis_label = 'Time'
+    fig4.xgrid.grid_line_color = None
+
+    return [fig1,fig2,fig3,fig4]
     
 #Update
 def update():
     new_src = make_dataset()
     source_city.data = dict(
-        x=new_src['MappedFixationPointX'],
-        y=new_src['MappedFixationPointY'],
-        timestamp=new_src['Timestamp'],
+        x=new_src['StimuliName'],
         station=new_src['StimuliName'],
-        fixation_duration=new_src['Fixation']
+        fixation_duration=new_src['FixationDuration']
     )
 
-select_city.on_change('value', lambda attr, old, new: update())
-selections = [select_city]
+select_city_1.on_change('value', lambda attr, old, new: update())
+select_city_2.on_change('value', lambda attr, old, new: update())
+
+selection_1 = [select_city_1]
+selection_2 = [select_city_2]
 
 plot = make_plot(source_city)
 
-widgets = column(*selections, width = 320, height = 200)
+widget_1 = column(*selection_1, width = 320, height = 200)
+widget_2 = column(*selection_2, width = 320, height = 200)
 
-layout = row(plot, widgets)
+layout = layout([
+                [plot[0], plot[1], widget_1],
+                [plot[2], plot[3], widget_2]
+])
 
 update()
 curdoc().add_root(layout)
