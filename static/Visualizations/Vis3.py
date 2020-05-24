cf032a44182dbf54ca7323bcf42629aee72aa2ff
ilkeyil <56206546+ilkeyil@users.noreply.github.com>
2020-05-24 11:30:59 +0200

    vis3 final changes for the interm submission

--- static/Visualizations/Vis3.py
+++ static/Visualizations/Vis3.py
@@ -1,8 +1,15 @@
+#Note to the examiner: This visualization is complete except not updating background image. 
+#In the future slider bars and other interactive methods will be added to allow user to customize the plotting.
+#Right now 3 slider bars are present but not yet been made interactive.
+
 import numpy as np  # import auxiliary library, typical idiom
 import pandas as pd
 import csv
 import math
 import seaborn as sns
+from PIL import Image
+import urllib3 as urllib
+import io
 import matplotlib.pyplot as plt
 from bokeh.layouts import gridplot, column, row, WidgetBox, layout
 from bokeh.plotting import figure, output_file, show, curdoc
@@ -11,6 +18,7 @@ from bokeh.models import Select, Tabs, Panel, Button, ColumnDataSource, Grid, Im
 from bokeh.models import Range1d, ColorBar, LinearColorMapper, LogColorMapper, LogTicker, Div, Select, Slider, TextInput
 from bokeh.models.callbacks import CustomJS
 from PIL import Image
+import PIL
 from bokeh.models.widgets import Dropdown
 from bokeh.transform import transform
 import sqlite3 as sql
@@ -31,25 +39,23 @@ src = ColumnDataSource(data = dict(x=[], y=[], timestamp=[], station=[], user=[]
 df_paths = df_paths.astype({'Timestamp': int, 'StimuliName': str, 'FixationIndex': float, 'FixationDuration': float, 
                             'MappedFixationPointX': int, 'MappedFixationPointY' : int, 'user': str, 'description': str})
 
-#p.image_url(url = ["https://www.jelter.net/stimuli/"+selectStation.value], x = 0 , y = 1500, w = 1500 , h = 1500)
-image = PIL.Image.open("sample.png")
-width, height = image.size
-close = (p*w*h) / (math.Pi)
-
+alp = 0.5 #transparency
+dia = 15 #size of circles
+#p_val = 0.1
+#dia = (p_val * width * height) / (math.Pi)
+dia2 = dia**2
 
 #Choosing the data we want
 def make_dataset():
     plot_data = df_paths[(df_paths['StimuliName'] == selectStation.value)].copy() # & (df_paths['user'] == selectUser.value)
-    #length = len(plot_data.index) #number of rows in the dataframe is calculated
 
-    plot_data = plot_data.sort_values(by=['MappedFixationPointX']).reset_index().drop('index', axis=1)
+    background = selectStation.value
+
+    plot_data = plot_data.sort_values(by=['MappedFixationPointX']).reset_index().drop('index', axis=1)#sort by x coordinate
    
     plot_data['closeness'] = 0 #the list is added to the dataframe as a new column named "closeness" and only contains 0s
 
-    close = 15
-    close2 = close**2
     for index, row in plot_data.iterrows(): #Iterates every row one by one
-        #closeness_val = int(row['closeness'])
         xi_val = int(row['MappedFixationPointX'])
         yi_val = int(row['MappedFixationPointY'])
         closeness_val = 0
@@ -57,62 +63,61 @@ def make_dataset():
                 if index != index2: #If the row found is not the same row, the distance between the two points is calculated.
                         xi2_val = int(OtherRow['MappedFixationPointX'])
                         yi2_val = int(OtherRow['MappedFixationPointY'])
-                        if (xi2_val - xi_val > close):
+                        if (xi2_val - xi_val > dia):
                             break
-                        if ( (((xi2_val - xi_val)**2 + (yi2_val - yi_val)**2 )) < close2 ) : #Checks if distance is 'close enough'.
+                        if ( (((xi2_val - xi_val)**2 + (yi2_val - yi_val)**2 )) < dia2 ) : #Checks if distance is 'close enough'.
                             closeness_val = closeness_val + 1 
-        plot_data.loc[index, 'closeness' ] = closeness_val #This line changes the actual row on the data frame
-               
+        plot_data.loc[index, 'closeness' ] = closeness_val #This line changes the actual row on the data frame  
     return plot_data
 
 #making the plot
 def make_plot(src):
     p = figure(
-        plot_width=990,
-        plot_height=720,
-        x_range=(0,1500), 
-        y_range=(0,1500),
+        plot_width = print_width,
+        plot_height = print_height,
+        x_range = (0, width), 
+        y_range = (height, 0), #y value is manipulated to have the correct coordinates.
+                                #in the dataset origin is treated to be the upper left corner. While graphing it is lower left. 
+                                #Therefore we "flip" the y axis in the dataset.           
         title = 'Heatmap',
         x_axis_label = 'x coordinate',
         y_axis_label = 'y coordinate'
     )
-
-    #dropwdown = ['hello', 'goodbye', ...]
-    #x = select.dropwdown
-    #image name = str(images/) + str(x)
-    #print(image_name)
-    
-    p.image_url(url = ["https://www.jelter.net/stimuli/"+selectStation.value], 
-                x = 0 , y = 1500, w = 1500 , h = 1500) #'../../' + 
-    #C:\Users\20190756\Documents\GitHub\DBL-Project\static\Visualizations\Stimuli
-
+    p.image_url(url = ["https://www.jelter.net/stimuli/" + selectStation.value], 
+                x = 0 , y = 0, w = width, h = height) #'../../' + 
     colors = ["#0000FF", "#0072FF", "#00FF00", "#D1FF00", "#FFC500", "#FF6C00", "#FF0000"]
     cmap = LinearColorMapper(palette=colors)
-    wid = 15
-    hei = wid*11/8
+    #hei = dia*ratio
     p.ellipse(x="x", y="y", source=src, line_color=None, 
-              fill_color=transform('closeness', cmap), width=wid, height=hei)
+              fill_color=transform('closeness', cmap), width=dia, height=dia, alpha=alp)
     color_bar = ColorBar(color_mapper=cmap, ticker=LogTicker(),
                      label_standoff=12, border_line_color=None, location=(0,0))
     p.add_layout(color_bar, 'right')
-
     return p
 
 #Select
 for station in df_paths['StimuliName']:
     stations.append(station)
-#for person in df_paths['user']:
-#    users.append(person)
-#to remove duplicates
 stations = list(dict.fromkeys(stations))
-#users = list(dict.fromkeys(users))
 
 selectStation = Select(title="Station:", value = '01_Antwerpen_S1.jpg', options=stations)
-#selectUser = Select(title='User:', value='p1', options=users)
-
-selectAlpha = Slider(title="Select the transparancy of the plotting", start=0, end=1, value=0.05, step=1)
-selectSize = Slider(title="Select the size of the dots", start=0, end=50, value=0.1, step=15) 
+selectAlpha = Slider(title="Select the transparancy of the plotting", start=0, end=1, value=0.05, step=0.01)
+selectSize = Slider(title="Select the size of the dots", start=0, end=50, value=0.1, step=1) 
 selectClose = Slider(title="Select the closeness value", start=0, end=1, value=0.1, step=0.01) #p-value in the equation
+
+callback = CustomJS(args=dict(source=src, alpha=selectAlpha, size=selectSize, close=selectClose),
+                    code="""
+    const data = source.data;
+    const alp_= alpha.value;
+    const dia = size.value;
+    const p_val = close.value;
+
+    source.change.emit();
+""")
+
+selectAlpha.js_on_change('value', callback)
+selectSize.js_on_change('value', callback)
+selectClose.js_on_change('value', callback)
 
 #Update
 def update():
@@ -127,17 +132,19 @@ def update():
     )
 
 selectStation.on_change('value', lambda attr, old, new: update())
-#selectUser.on_change('value', lambda attr, old, new: update())
-selections = [selectStation] #, selectUser
+selections = [selectStation]
 
-#image_name = str('Stimuli/') + str(selectStation.value)
-#image_name = str(image_name)
-#C:\Users\20190756\Documents\GitHub\DBL-Project\static\Visualizations\Stimuli
-#print(image_name)
+image = PIL.Image.open('Stimuli/' + selectStation.value)
+width, height = image.size
 
-#widgets = column(*selections, width = 320, height = 200)
-widgets = column(selectStation) #, selectUser
+ratio = width/height
+
+print_width = int(ratio * 720)
+print_height = int(720)
+
+widgets = column(selectStation)
 plot = make_plot(src)
-layout = row(plot, widgets)
+layout = row(plot, 
+             column( widgets, selectAlpha, selectSize, selectClose))
 update()
 curdoc().add_root(layout)
