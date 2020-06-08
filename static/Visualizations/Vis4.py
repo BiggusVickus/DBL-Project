1e698170018c0f524b750e4acfc3275c8697f9ed
Mark van der Meer <m.j.v.d.meer@student.tue.nl>
2020-06-08 11:21:23 +0200

    Update Vis4.py

--- static/Visualizations/Vis4.py
+++ static/Visualizations/Vis4.py
@@ -12,7 +12,7 @@ from bokeh.transform import dodge
 from math import pi
 
 #Reading csv file
-df_map = pd.read_csv('Datasets/fixation_data.csv', parse_dates=[0])
+df_map = pd.read_csv('Uploads/fixation_data.csv', parse_dates=[0])
 df_map = df_map.astype({'Timestamp': int, 'StimuliName': str, 'FixationIndex': float, 'FixationDuration': float, 'MappedFixationPointX': int, 'MappedFixationPointY' : int, 'user': str, 'description': str})
 
 #Making data frames
@@ -29,20 +29,10 @@ df_map_S1_gray_grouped = df_map_S1_gray.groupby("StimuliName")
 df_map_S2_color_grouped = df_map_S2_color.groupby("StimuliName")
 df_map_S2_gray_grouped = df_map_S2_gray.groupby("StimuliName")
 
-df_map_S1_color_grouped_2 = df_map_S1_color.groupby(["StimuliName","user"])
-df_map_S1_gray_grouped_2 = df_map_S1_gray.groupby(["StimuliName","user"])
-df_map_S2_color_grouped_2 = df_map_S2_color.groupby(["StimuliName","user"])
-df_map_S2_gray_grouped_2 = df_map_S2_gray.groupby(["StimuliName","user"])
-
 S1_color = df_map_S1_color_grouped[['FixationDuration']].sum().reset_index()
 S1_gray = df_map_S1_gray_grouped[['FixationDuration']].sum().reset_index()
 S2_color = df_map_S2_color_grouped[['FixationDuration']].sum().reset_index()
 S2_gray = df_map_S2_gray_grouped[['FixationDuration']].sum().reset_index()
-
-S1_color_2 = df_map_S1_color_grouped_2[['FixationDuration']].sum().reset_index()
-S1_gray_2 = df_map_S1_gray_grouped_2[['FixationDuration']].sum().reset_index()
-S2_color_2 = df_map_S2_color_grouped_2[['FixationDuration']].sum().reset_index()
-S2_gray_2 = df_map_S2_gray_grouped_2[['FixationDuration']].sum().reset_index()
 
 list_1 = S1_color['StimuliName'].tolist()
 list_2 = S1_gray['StimuliName'].tolist()
@@ -50,10 +40,10 @@ list_3 = S2_color['StimuliName'].tolist()
 list_4 = S2_gray['StimuliName'].tolist()
 
 #Make ColumnDataSource
-source_city_1 = ColumnDataSource(data = dict(x=[], station=[], fixation_duration=[], user=[]))
-source_city_2 = ColumnDataSource(data = dict(x=[], station=[], fixation_duration=[], user=[]))
-source_city_3 = ColumnDataSource(data = dict(x=[], station=[], fixation_duration=[], user=[]))
-source_city_4 = ColumnDataSource(data = dict(x=[], station=[], fixation_duration=[], user=[]))
+source_city_1 = ColumnDataSource(data = dict(x=[], station=[], fixation_duration=[]))
+source_city_2 = ColumnDataSource(data = dict(x=[], station=[], fixation_duration=[]))
+source_city_3 = ColumnDataSource(data = dict(x=[], station=[], fixation_duration=[]))
+source_city_4 = ColumnDataSource(data = dict(x=[], station=[], fixation_duration=[]))
 
 #Global variables
 Stations_S1_Color = []
@@ -110,23 +100,12 @@ select_city_4 = Select(
     options = Stations_S2_Gray
 )
 
-select_city_6 = Select(
-    title = 'Choose city S2 Gray',
-    value = '01_Antwerpen_S2.jpg',
-    options = Stations_S2_Color
-)
-
-checkbox_group = CheckboxGroup(
-    labels = Stations_S1_Color, active = [0]
-)  
-
-
 def make_dataset_1():
-    plot_data_S1_color = S1_color[(S1_color['StimuliName'] == checkbox_group.active)].copy()
+    plot_data_S1_color = S1_color[(S1_color['StimuliName'] == select_city_1.value)].copy()
     return plot_data_S1_color
 
 def make_dataset_2():
-    plot_data_S1_gray = S1_gray[(S1_gray['StimuliName'] == Stations_S1_Color)].copy()
+    plot_data_S1_gray = S1_gray[(S1_gray['StimuliName'] == select_city_2.value)].copy()
     return plot_data_S1_gray
 
 def make_dataset_3():
@@ -137,27 +116,25 @@ def make_dataset_4():
     plot_data_S2_gray = S2_gray[(S2_gray['StimuliName'] == select_city_4.value)].copy()
     return plot_data_S2_gray
 
-      
-fig1 = figure(
-    x_range = list_1,
-    y_range = (0,750000),
-    title = 'S1 Color Barcharts',
-    plot_width = 500,
-    plot_height = 500
-)
-bar = fig1.vbar(x='x', top = 'fixation_duration', width = 0.5, source = source_city_1, color = "red", legend_label = "color")
-fig1.xaxis.axis_label = 'City'
-fig1.yaxis.axis_label = 'Total Time'
-fig1.xaxis.major_label_orientation = pi/3
-fig1.xgrid.grid_line_color = None
-fig1.add_tools(HoverTool(
+
+def make_plot_1(src):
+    fig1 = figure(
+        x_range = list_1,
+        y_range = (0,750000),
+        title = 'S1 Color Barcharts',
+        plot_width = 500,
+        plot_height = 500
+    )
+    bar = fig1.vbar(x='x', top = 'fixation_duration', width = 0.5, source = source_city_1, color = "red", legend_label = "color")
+    fig1.xaxis.axis_label = 'City'
+    fig1.yaxis.axis_label = 'Total Time'
+    fig1.xaxis.major_label_orientation = pi/3
+    fig1.xgrid.grid_line_color = None
     tooltips=[
         ("Total Time", "@fixation_duration")
         #("p1","@dsfdf")
     ], renderers=[bar]
-))
-
-bars = [bar]
+    return [fig1]
 
 def make_plot_2(src):
     fig2 = figure(
@@ -205,7 +182,7 @@ def make_plot_4(src):
     return [fig4]
 
 #Update
-def update_1(attr, old, new):
+def update_1():
     new_src = make_dataset_1()
     source_city_1.data = dict(
         x=new_src['StimuliName'],
@@ -213,7 +190,7 @@ def update_1(attr, old, new):
         fixation_duration=new_src['FixationDuration']
     )
 
-def update_2(attr, old, new):
+def update_2():
     new_src_2 = make_dataset_2()
     source_city_2.data = dict(
         x=new_src_2['StimuliName'],
@@ -237,33 +214,28 @@ def update_4():
         fixation_duration=new_src_4['FixationDuration']
     )
 
-
-#select_city_1.on_change('value', lambda attr, old, new: update_1())
-#select_city_2.on_change('value', lambda attr, old, new: update_2())
+select_city_1.on_change('value', lambda attr, old, new: update_1())
+select_city_2.on_change('value', lambda attr, old, new: update_2())
 select_city_3.on_change('value', lambda attr, old, new: update_3())
 select_city_4.on_change('value', lambda attr, old, new: update_4())
 
-checkbox_group.on_change('active',update_2)
-
-#selection_1 = [select_city_1]
-#selection_2 = [select_city_2]
+selection_1 = [select_city_1]
+selection_2 = [select_city_2]
 selection_3 = [select_city_3]
 selection_4 = [select_city_4]
 
-selection_6 = [checkbox_group]
-
-#plot_1 = make_plot_1(source_city_1)
+plot_1 = make_plot_1(source_city_1)
 plot_2 = make_plot_2(source_city_2)
 plot_3 = make_plot_3(source_city_3)
 plot_4 = make_plot_4(source_city_4)
 
-widget_1_2 = column(*selection_6, width = 320, height = 200)
+widget_1_2 = column(*selection_1, *selection_2, width = 320, height = 200)
 widget_3_4 = column(*selection_3, *selection_4, width = 320, height = 200)
 
 layout = layout([
-                [plot_2[0], widget_1_2],
+                [plot_1[0], plot_2[0], widget_1_2],
                 [plot_3[0], plot_4[0], widget_3_4]
 ])
 
-update_1, update_2, update_3(), update_4()
+update_1(), update_2(), update_3(), update_4()
 curdoc().add_root(layout)
